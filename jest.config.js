82b2b5c59b31a818ae2372434c786dc690ca28e6
jellasaikiran <saikiran.jella@zemosolabs.com>
2023-01-20 18:21:13 +0530

    Testing Library Setup

---
+++ jest.config.js
@@ -0,0 +1,35 @@
+/*
+ * For a detailed explanation regarding each configuration property, visit:
+ * https://jestjs.io/docs/en/configuration.html
+ */
+
+module.exports = {
+    testEnvironment: 'jsdom',
+    coverageDirectory: 'coverage',
+    collectCoverage: true,
+    collectCoverageFrom: ['src/**/*.{ts,tsx}', '!src/**/*.stories.{ts,tsx}'],
+    clearMocks: true,
+    setupFilesAfterEnv: ['./src/SetupTests.ts'],
+    coveragePathIgnorePatterns: [
+      '<rootDir>/node_modules/',
+      '.story.js',
+      '<rootDir>/coverage',
+      '<rootDir>/src/utils',
+      '<rootDir>/src/mocks',
+      '<rootDir>/src/theme',
+      'styles.ts',
+      'styles.tsx',
+      '<rootDir>/src/*.d.ts',
+    ],
+    testPathIgnorePatterns: ['<rootDir>/node_modules/', '<rootDir>/coverage'],
+    transform: {
+      '\\.[jt]sx?$': 'babel-jest',
+    },
+    moduleNameMapper: {
+      '.+\\.(svg|css|styl|less|sass|scss|png|jpg|ttf|woff|woff2)$':
+        'jest-transform-stub',
+    },
+    verbose: true,
+    moduleDirectories: ['node_modules', 'src'],
+  }
+  
